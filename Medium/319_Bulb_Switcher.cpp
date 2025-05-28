a1bafa10d415c03e3cde5f7d4671baefa3e1e456
DucMinhh1234567 <163238151+DucMinhh1234567@users.noreply.github.com>
2025-05-29 01:21:15 +0700

    I need to learn more about basic math and patterns :').

---
+++ Medium/319_Bulb_Switcher.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
