e7f8f7fe4f5bf222ffec9f5d6bd755294469b36c
Trần ĐMinh <tducminh802@gmail.com>
2026-02-04 13:27:16 +0700

    Slow return

--- Easy/1668_Maximum Repeating Substring.cpp
+++ Easy/1668_Maximum Repeating Substring.cpp
@@ -10,13 +10,28 @@
 
 using namespace std;
 
+// For a string sequence, a string word is k-repeating 
+// if word concatenated k times is a substring of sequence. 
+// The word's maximum k-repeating value is the highest value k 
+// where word is k-repeating in sequence. If word is not a substring of sequence, 
+// word's maximum k-repeating value is 0.
+
+// Given strings sequence and word, 
+// return the maximum k-repeating value of word in sequence.
+
+// Example 1:
+
+// Input: sequence = "ababc", word = "ab"
+// Output: 2
+// Explanation: "abab" is a substring in "ababc".
+
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
         int maxCount = 0;
         int n = sequence.size();
         int m = word.size();
-        
+
         // Try starting from each position
         for (int i = 0; i <= n - m; i++) {
             int count = 0;
