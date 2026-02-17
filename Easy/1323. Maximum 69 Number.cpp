b01aa3d0a23310940ad9da7246ef6e498de45b77
Trần ĐMinh <tducminh802@gmail.com>
2026-02-17 23:24:10 +0700

    easy

---
+++ Easy/1323. Maximum 69 Number.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+
+// You are given a positive integer num 
+// consisting only of digits 6 and 9.
+// Return the maximum number you can get by 
+// changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+// Example 1:
+// Input: num = 9669
+// Output: 9969
+// Explanation: 
+// Changing the first digit results in 6669.
+// Changing the second digit results in 9969.
+// Changing the third digit results in 9699.
+// Changing the fourth digit results in 9666.
+// The maximum number is 9969.
+
+// Example 2:
+// Input: num = 9996
+// Output: 9999
+// Explanation: Changing the last digit 6 to 9 results in the maximum number.
+
+// Example 3:
+// Input: num = 9999
+// Output: 9999
+// Explanation: It is better not to apply any change.
+
+// Constraints: 
+// 1 <= num <= 10^4
+// num consists of only 6 and 9 digits.
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string temp = to_string(num);
+        for (int i = 0; i < temp.size(); i++){
+            if (temp[i] != '9') {
+                temp[i] = '9';
+                break;
+            }
+        }
+
+        return stoi(temp);
+    }
+};
